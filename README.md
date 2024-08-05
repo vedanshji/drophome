4b1770a9e7a41a3d92395c6bbf3d60091d098703
VEDANSH RAJ <gswip.vedanshr@aicofindia.com>
2024-08-05 15:41:10 +0530

    firt one

---
+++ README.md
@@ -0,0 +1,5 @@
+## Fitness Website
+
+GitHub Pages : [Active](https://Mr-vabs.github.io/fitness-website/index.html)
+
+SoloLearn : [Active](https://code.sololearn.com/WdCOC57jmDBa/?ref=app)
---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+JS-FREE code ;(
+sorry , let me add a alert :-)
+
+
+easiest and simple try i guess. beginners should try such type of codes xD
+
+
+
+alert("upvote it if you liked this code xD. \nI've made this for my mini project.\n\nHere's the live link hosted with github pages \n\n https://Mr-vabs.github.io/fitness-website/index.html \n\n https://code.sololearn.com/WdCOC57jmDBa/?ref=app \n\n\n -------- vebhv | vaibhav | vabrish ---------");
+
+/* copy it IF YOU'LL USE IT FOR  just for reference  ELSE YOU AREN'T ALLOWED TO COPY */
